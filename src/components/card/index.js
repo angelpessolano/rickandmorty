280e2ca9ccb4ea34dec09ca2e88e3699789dee15
Angel Pessolano <angel.pessolano@gmail.com>
2023-08-28 09:26:48 -0400

    Cambios liked vs unliked character

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -29,7 +29,11 @@ const Card = ({ data, id_c }) => {
   return (
     <ul className="w-[375px] pl-2">
       {data.characters.results.map(({ id, name, species, image, gender }) => (
-        <li key={id} onClick={() => handlecharacter(id)}>
+        <>
+          {context.characterlike.indexOf(id) !== -1 ? (
+            <></>
+          ) : (
+            <li key={id} onClick={() => handlecharacter(id)}>
           <div className="py-2 px-2 border-y border-slate-100  bg-white  hover:bg-primary-100  hover:rounded-xl space-y-2
             flex sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img
@@ -47,20 +51,8 @@ const Card = ({ data, id_c }) => {
                 focus:ring-2 focus:ring-secondary-600 focus:ring-offset-2"
                 onClick={() => elementlist(id)}
               >
-                {context.characterlike.indexOf(id) !== -1 ? (
-                  <svg
-                    className="w-[1.15rem] h-[1.15rem] text-green-500"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 18"
-                  >
-                    <path d="M17.947 2.053a5.209 5.209 0 0 0-3.793-1.53A6.414 6.414 0 0 0 10 2.311 6.482 6.482 0
-                     0 0 5.824.5a5.2 5.2 0 0 0-3.8 1.521c-1.915 1.916-2.315 5.392.625 8.333l7 
-                     7a.5.5 0 0 0 .708 0l7-7a6.6 6.6 0 0 0 2.123-4.508 5.179 5.179 0 0 0-1.533-3.793Z" />
-                  </svg>
-                ) : (
-                  <svg
+                
+                <svg
                     className="w-6 h-6"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
@@ -75,13 +67,20 @@ const Card = ({ data, id_c }) => {
                       d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z"
                     />
                   </svg>
-                )}
+              
               </button>
             </div>
           </div>
         </li>
+          )}
+       
+
+
+        </>
+
       ))}
     </ul>
+    
   );
 };
 export default Card;
